2a0995912bb8bfaff7ffbfbc119e7be5b25821c3
epicoderr <nikolasrbinoski6@gmail.com>
2025-03-26 14:03:51 +0200

    Task 1: Add payment_card project structure and PaymentCard class

---
+++ exercises/week2/payment_card/src/payment_card.py
@@ -0,0 +1,31 @@
+# Prices are in cents
+CHEAP = 250
+YUMMY = 400
+
+
+class PaymentCard:
+    def __init__(self, balance):
+        # The balance is in cents
+        self.balance = balance
+
+    def eat_cheap(self):
+        if self.balance >= CHEAP:
+            self.balance -= CHEAP
+
+    def eat_yummy(self):
+        if self.balance >= YUMMY:
+            self.balance -= YUMMY
+
+    def add_money(self, amount):
+        if amount < 0:
+            return
+
+        self.balance += amount
+
+        if self.balance > 15000:
+            self.balance = 15000
+
+    def __str__(self):
+        balance_in_euros = round(self.balance / 100, 2)
+
+        return "The card has {:0.2f} euros on it".format(balance_in_euros)
